4905b79f305a4b059134cc4208b710a64850e168
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2022-11-27 21:16:56 +0000

    latest commit

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ const heading = document.querySelector("h1")
 const playerX = "x";
 const playerO = "o";
 
-const winnerArray = []
+let winnerArray = []
 
 let currentPlayer = playerX;
 
 const winningCombination = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
+  ["0", "1", "2"],
+  ["3", "4", "5"],
+  ["6", "7", "8"],
+  ["0", "3", "6"],
+  ["1", "4", "7"],
+  ["2", "5", "8"],
+  ["0", "4", "8"],
+  ["2", "4", "6"],
 ];
 
 // FUNCTION TO START THE GAME WITH CLICK EVENTS ON THE CELLS!!
@@ -37,21 +37,31 @@ boxClicked = (event) => {
   if (currentPlayer == playerX) {
     event.target.innertext = currentPlayer;
     winnerArray.push(event.target.id)
-    console.log(winnerArray)
   }
+
   if (currentPlayer == playerO) {
     currentPlayer = playerX;
   } else {
-    
     currentPlayer = playerO;
   }
     event.target.innerHTML = currentPlayer;
+
+    playerHasWon()
+    
+    
 };
 
 const playerHasWon = () => {
+        if (winnerArray == winningCombination[0]) {
+            alert("winner")
+        }
+        else {
+            console.log(winnerArray)
+            console.log(winningCombination[0])
+        }
+    }
 
-
-}
+console.log(winningCombination[0])
 
 
 const restart = () => {
